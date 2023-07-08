8052601d1d5d1ca7e7370680ac3aa045b4540eaf
bonfire203 <46801720+bonfire203@users.noreply.github.com>
2023-07-08 02:46:19 -0600

    Nav Mesh w/ State Machine
    implemented and tested!

--- Torchfire-GMTK/Assets/Agent/AgentPatrolAction.cs
+++ Torchfire-GMTK/Assets/Agent/AgentPatrolAction.cs
@@ -16,10 +16,12 @@ namespace myAgentFSM
             var navMeshAgent = stateMachine.GetComponent<NavMeshAgent>();
             var randomPointOnNavMesh = stateMachine.GetComponent<RandomPointOnNavMesh>();
             Vector3 point;
+            
 
-            if(randomPointOnNavMesh.RandomPoint(stateMachine.transform.position, 2f, out point))
+            if (randomPointOnNavMesh.RandomPoint(stateMachine.transform.position, 2f, out point))
             {
                 Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                navMeshAgent.destination = point;
             }
             //navMeshAgent.setDestination()
         }
--- Torchfire-GMTK/Assets/Agent/AgentState.cs
+++ Torchfire-GMTK/Assets/Agent/AgentState.cs
@@ -7,20 +7,16 @@ namespace AgentFSM
     [CreateAssetMenu(menuName = "FSM/State")]
     public sealed class AgentState : AgentBaseState
     {
-        [CreateAssetMenu(menuName = "FSM/State")]
-        public sealed class State : AgentBaseState
+        public List<AgentFSMAction> Action = new List<AgentFSMAction>();
+        public List<AgentTransition> Transitions = new List<AgentTransition>();
+
+        public override void Execute(AgentBaseStateMachine machine)
         {
-            public List<AgentFSMAction> Action = new List<AgentFSMAction>();
-            public List<AgentTransition> Transitions = new List<AgentTransition>();
+            foreach(var action in Action)
+                action.Execute(machine);
 
-            public override void Execute(AgentBaseStateMachine machine)
-            {
-                foreach(var action in Action)
-                    action.Execute(machine);
-
-                foreach(var transition in Transitions)
-                    transition.Execute(machine);
-            }
+            foreach(var transition in Transitions)
+                transition.Execute(machine);
         }
     }
 }
